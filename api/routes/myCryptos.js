4c0c65fbae3600926eb1bdec32a50ace384f9da5
Arth K. Gajjar <iarthstar@gmail.com>
2019-09-11 21:38:52 +0530

    Updated : routes/myCryptos.js with catch & logging

--- api/routes/myCryptos.js
+++ api/routes/myCryptos.js
@@ -2,27 +2,34 @@ const utils = require("../../utils");
 const axios = require('axios');
 
 exports.getPriceForCryptos = (method, req, res) => {
-    let options = {};
+    
+    try {
+        let options = {};
 
-    switch (method) {
-        case "post": {
-            const { cryptoNames, currencies } = req.body;
-            options.url = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptoNames.join(",")}&tsyms=${currencies.join(",")}`;
-        } break;
+        switch (method) {
+            case "post": {
+                const { cryptoNames, currencies } = req.body;
+                options.url = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptoNames.join(",")}&tsyms=${currencies.join(",")}`;
+            } break;
 
-        case "get": {
-            const { cryptoNames, currencies } = req.params;
-            options.url = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptoNames}&tsyms=${currencies}`;
-        } break;
+            case "get": {
+                const { cryptoNames, currencies } = req.params;
+                options.url = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptoNames}&tsyms=${currencies}`;
+            } break;
+        }
+
+        options.method = "get";
+
+        axios(options)
+            .then(response => {
+                return res.send(response.data);
+            }).catch(err => {
+                utils.error(err);
+                return res.json({ error: false, error_message: "Something went wrong..." }).status(404);
+            });
+    } catch (err) {
+        utils.error(err);
+        return res.json({ error: true, error_message: "Something went wrong..." }).status(404);
     }
-
-    options.method = "get";
-
-    axios(options)
-        .then(response => {
-            return res.send(response.data);
-        }).catch(err => {
-            return res.json({ err }).status(404);
-        });
 
 };
